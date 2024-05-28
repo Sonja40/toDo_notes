f04870ed4edffed3de29572aadc78968dcf486e4
SonjaReimannThanisch <137377644+Sonja40@users.noreply.github.com>
2024-05-28 22:01:08 +0200

    content wrap html, footer css improved and size of the input container

---
+++ src/script.js
@@ -0,0 +1,8 @@
+function showBurgerMenu() {
+    let checkBox = document.getElementById('checkbox');
+    if (checkBox.checked == true) {
+        document.getElementById('menu').classList.add('show-burgerMenu')
+    } else {
+        document.getElementById('menu').classList.remove('show-burgerMenu')
+    }
+}
