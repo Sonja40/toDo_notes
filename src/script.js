16c56ab253e17b03978a381763343e83c6f3a6bc
SonjaReimannThanisch <137377644+Sonja40@users.noreply.github.com>
2024-05-28 22:47:47 +0200

    function for data LocalStorage save & load

--- src/script.js
+++ src/script.js
@@ -7,3 +7,32 @@ function showBurgerMenu() {
     }
 }
 
+let content = [];
+let entry = [];
+let removeContent = [];
+let removeEntry = [];
+load();
+
+function load() {
+    let contentastext = localStorage.getItem('content');
+    let entryasText = localStorage.getItem('entry');
+    let removeContentasText = localStorage.getItem('removeContent');
+    let removeEntryasText = localStorage.getItem('removeEntry');
+    if (contentastext && entryasText && removeContentasText && removeEntryasText) {
+        content.JSON.parse(contentastext);
+        entry.JSON.parse(entryasText);
+        removeContent.JSON.parse(removeContentasText);
+        removeEntry.JSON.parse(removeEntryasText);
+    }
+}
+
+function save() {
+    let contentastext = JSON.stringify(content);
+    let entryasText = JSON.stringify(entry);
+    let removeContentasText = JSON.stringify(removeContent);
+    let removeEntryasText =JSON.stringify(removeEntry);
+    localStorage.setItem('content', contentastext);
+    localStorage.setItem('entry', entryasTextastext);
+    localStorage.setItem('removeContent', removeContentasText);
+    localStorage.setItem('removeEntry', removeEntryasText);
+}
