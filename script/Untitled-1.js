fe6a4d824d8d5049cc52adf700c03e3c60d0b614
SonjaReimannThanisch <137377644+Sonja40@users.noreply.github.com>
2024-06-04 17:47:26 +0200

    done

--- script/Untitled-1.js
+++ script/Untitled-1.js
@@ -1,5 +1,5 @@
 // Funktion zum Laden der Notizen aus dem Local Storage
-function load() {
+/*function load() {
     if (localStorage.getItem('titles')) {
         titles = JSON.parse(localStorage.getItem('titles'));
         texts = JSON.parse(localStorage.getItem('texts'));
@@ -9,17 +9,18 @@ function load() {
         removetexts = JSON.parse(localStorage.getItem('removetexts'));
     }
 }
+*/
 
 // Funktion zum Speichern der Notizen im Local Storage
-function save() {
+ /* function save() {
     localStorage.setItem('titles', JSON.stringify(titles));
     localStorage.setItem('texts', JSON.stringify(texts));
     localStorage.setItem('removetitles', JSON.stringify(removetitles));
     localStorage.setItem('removetexts', JSON.stringify(removetexts));
-}
+} */
 
 // Funktion zum Entfernen einer Notiz und Verschieben in den Papierkorb
-function removeNote(i) {
+/*function removeNote(i) {
     if (document.getElementById('headline').innerHTML === 'Today´s To Do´s') {
         removetitles.push(titles[i]);
         removetexts.push(texts[i]);
@@ -32,9 +33,9 @@ function removeNote(i) {
     render();
     save();
 }
-
+*/
 // Funktion zum Wiederherstellen einer Notiz aus dem Papierkorb
-function restore(i) {
+/*function restore(i) {
     titles.push(removetitles[i]);
     texts.push(removetexts[i]);
     removetitles.splice(i, 1);
@@ -42,9 +43,9 @@ function restore(i) {
     render();
     save();
 }
-
+*/
 // Funktion zum Rendern der Notizen
-function render() {
+/*function render() {
     let noteBox = document.getElementById('notes');
     noteBox.innerHTML = ``;
     if (document.getElementById('headline').innerHTML === 'Today´s To Do´s') {
@@ -60,10 +61,10 @@ function render() {
             noteBox.innerHTML += newTrashNote(i, title, text);
         }
     }
-}
+}*/
 
 // Funktion zum Erstellen des HTML-Codes für eine neue Notiz
-function newNote(i, title, text) {
+/*function newNote(i, title, text) {
     return `
     <div class="note">
     <a href="#"><img src="./img/pin.png" alt="Save Pin" class="input-icon"></a>
@@ -72,10 +73,10 @@ function newNote(i, title, text) {
     <textarea class="noteText" onclick="editNote(${i})">${text}</textarea>
     </div>
     `;
-}
+}*/
 
 // Funktion zum Erstellen des HTML-Codes für eine neue Notiz im Papierkorb
-function newTrashNote(i, title, text) {
+/*function newTrashNote(i, title, text) {
     return `
     <div class="note">
     <a href="#"><img src="./img/behalter.png" alt="Wiederherstellen" onclick="restore(${i})" class="input-icon"></a>
@@ -88,3 +89,142 @@ function newTrashNote(i, title, text) {
 // Aufruf der Ladefunktion beim Start
 load();
 render();
+*/
+/*
+let titles = [];
+let notes = [];
+let titlesTrash = [];
+let notesTrash = [];
+load();
+*/
+/*
+function showBurgerMenu() {
+    let checkBox = document.getElementById('checkbox');
+    if (checkBox.checked) {
+        document.getElementById('menu').classList.add('show-burgerMenu');
+    } else {
+        document.getElementById('menu').classList.remove('show-burgerMenu');
+    }
+}
+*/
+/*
+function load() {
+    titles = JSON.parse(localStorage.getItem('titles')) || [];
+    notes = JSON.parse(localStorage.getItem('notes')) || [];
+    titlesTrash = JSON.parse(localStorage.getItem('titlesTrash')) || [];
+    notesTrash = JSON.parse(localStorage.getItem('notesTrash')) || [];
+}
+*/
+/*
+function save() {
+    localStorage.setItem('titles', JSON.stringify(titles));
+    localStorage.setItem('notes', JSON.stringify(notes));
+    localStorage.setItem('titlesTrash', JSON.stringify(titlesTrash));
+    localStorage.setItem('notesTrash', JSON.stringify(notesTrash));
+}
+*/
+/*
+function renderNotes() {
+    let noteBox = document.getElementById('notes');
+    noteBox.innerHTML = '';
+    if (document.getElementById('headline').innerHTML === 'Today´s To Do´s') {
+        for (let i = 0; i < titles.length; i++) {
+            noteBox.innerHTML += createNoteHTML(i, titles[i], notes[i], false);
+        }
+    } else {
+        for (let i = 0; i < titlesTrash.length; i++) {
+            noteBox.innerHTML += createNoteHTML(i, titlesTrash[i], notesTrash[i], true);
+        }
+    }
+}
+*/
+
+/*
+function createNoteHTML(i, title, note, isTrash) {
+    return `
+    <div class="note">
+        <a><img id="notePin" src="./img/green_pin.png"></a>
+        <a><img id="deleteImg" src="./img/rubbish-6664779_640.png" onclick="${isTrash ? `deleteForever(${i})` : `deleteNote(${i})`}"></a>
+        ${isTrash ? `<a><img id="restoreImg" src="./img/restore_from_trash.png" onclick="restore(${i})"></a>` : ''}
+        <h2 onclick="edit(${i}, ${isTrash})">${title}</h2>
+        <textarea class="noteText" ${isTrash ? 'readonly' : `onclick="edit(${i}, ${isTrash})"`}>${note}</textarea>
+    </div>`;
+}
+*/
+/*
+function addNote() {
+    let title = document.getElementById('title');
+    let note = document.getElementById('note');
+    titles.push(title.value);
+    notes.push(note.value);
+    title.value = '';
+    note.value = '';
+    save();
+    renderNotes();
+}
+*/
+/*
+function deleteNote(i) {
+    titlesTrash.push(titles[i]);
+    notesTrash.push(notes[i]);
+    titles.splice(i, 1);
+    notes.splice(i, 1);
+    save();
+    renderNotes();
+}
+*/
+/*
+function deleteForever(i) {
+    titlesTrash.splice(i, 1);
+    notesTrash.splice(i, 1);
+    save();
+    renderNotes();
+}
+*/
+/*
+function restore(i) {
+    titles.push(titlesTrash[i]);
+    notes.push(notesTrash[i]);
+    deleteForever(i);
+}
+*/
+/*
+function edit(i, isTrash) {
+    let noteBox = document.getElementById('edit');
+    showEdit();
+    let title = isTrash ? titlesTrash[i] : titles[i];
+    let note = isTrash ? notesTrash[i] : notes[i];
+    noteBox.innerHTML = `
+    <textarea class="titleEdit" id="titleEdit" cols="40" rows="5">${title}</textarea>
+    <textarea class="noteEdit" id="noteEdit" cols="40" rows="5">${note}</textarea>
+    <a><img id="notePinEdit" src="./img/green_pin.png" onclick="saveEdit(${i}, ${isTrash})"></a>`;
+}
+*/
+/*
+function saveEdit(i, isTrash) {
+    let title = document.getElementById('titleEdit').value;
+    let note = document.getElementById('noteEdit').value;
+    if (isTrash) {
+        titlesTrash[i] = title;
+        notesTrash[i] = note;
+    } else {
+        titles[i] = title;
+        notes[i] = note;
+    }
+    hideEdit();
+    save();
+    renderNotes();
+}
+*/
+/*
+function showEdit() {
+    document.getElementById('edit').classList.add('editShow');
+}
+
+function hideEdit() {
+    document.getElementById('edit').classList.remove('editShow');
+}
+
+load();
+renderNotes();
+*/
--- script/script.js
+++ script/script.js
@@ -13,135 +13,135 @@ let removetitles = [];
 let removetexts = [];
 load();
 
+function load() {
+    titles = JSON.parse(localStorage.getItem('titles')) || [];
+    texts = JSON.parse(localStorage.getItem('texts')) || [];
+    removetitles = JSON.parse(localStorage.getItem('removetitles')) || [];
+    removetexts = JSON.parse(localStorage.getItem('removetexts')) || [];
+}
+
+function save() {
+    localStorage.setItem('titles', JSON.stringify(titles));
+    localStorage.setItem('texts', JSON.stringify(texts));
+    localStorage.setItem('removetitles', JSON.stringify(removetitles));
+    localStorage.setItem('removetexts', JSON.stringify(removetexts));
+}
+
 function render() {
     let noteBox = document.getElementById('notes');
-    noteBox.innerHTML = ``;
+    noteBox.innerHTML = '';
     if (document.getElementById('headline').innerHTML == 'Today´s To Do´s') {
         for (let i = 0; i < titles.length; i++) {
-            const title = titles[i];
-            const text = texts[i];
-            noteBox.innerHTML += newNote(i, noteBox, title, text);
+            noteBox.innerHTML += newNote(i, titles[i], texts[i], false);
         }
     } 
     else {
         for (let i = 0; i < removetitles.length; i++) {
-            const title = removetitles[i];
-            const text = removetexts[i];
-            noteBox.innerHTML += newNote(i, noteBox, title, text);
+            noteBox.innerHTML += newTrashNote(i, removetitles[i], removetexts[i], true);
         }
     }
 }
 
-function newNote(i, noteBox, title, text) {
+function newNote(i, title, text, isTrash) {
     return `
     <div class="note">
-    <a href="#"><img src="./img/pin.png" alt="Save Pin" id="notePin" ></a>
-    <a href="#"><img src="./img/behalter.png" alt="Papierkorb" onclick="removeNote(${i})" id="deleteImg"></a>
-    <a href="#"><img src="./img/daten-wiederherstellen.png" alt="Papierkorb" onclick="removeNote(${i})" id="restoreImg"></a>
-    <h2 onclick="editNote(${i})">${title}</h2>
-    <textarea class="noteText" onclick="editNote(${i})">${text}</textarea>
+    <a href="#">
+        <img src="./img/pin.png" alt="Pin" id="notePin">
+    </a>
+    <a href="#"> 
+        <img src="./img/behalter.png" alt="Papierkorb" id="deleteImg" onclick="removeNote(${i})">
+    </a>
+    <a href="#">
+        <img src="./img/daten-wiederherstellen.png" alt="Zurück" id="restoreImg" class="img" onclick="restore(${i})">
+    </a>
+    <h2>${title}</h2>
+    <textarea class="noteText" readonly>${text}</textarea>
     </div>
-    `
+
+    `;
+
+}
+
+function newTrashNote(i, title, text) {
+    return `
+    <div class="note">
+    <a href="#">
+        <img src="./img/pin.png" alt="Pin" id="notePin">
+    </a>
+    <a href="#"> 
+        <img src="./img/behalter.png" alt="Papierkorb" id="deleteImg" onclick="deleteForever(${i})">
+    </a>
+    <a href="#">
+        <img src="./img/daten-wiederherstellen.png" alt="Zurück" id="restoreImg" onclick="restore(${i})">
+    </a>
+    <h2>${title}</h2>
+    <textarea class="noteText" readonly>${text}</textarea>
+    </div>
+    `;
 }
 
 function addNote() {
-    let titleInput = document.getElementById('title');
-    let textInput = document.getElementById('text');
+    let title = document.getElementById('title');
+    let text = document.getElementById('text');
 
-    titles.push(titleInput.value);
-    texts.push(textInput.value);
-
-    titleInput.value = '';
-    textInput.value = '';
+    titles.push(title.value);
+    texts.push(text.value);
+    title.value = '';
+    text.value = '';
 
     render();
     save();
-}
-
-function load() {
-    let titleAstext = localStorage.getItem('titles');
-    let textAstext = localStorage.getItem('texts');
-    let removetitlesAsText = localStorage.getItem('removetitles');
-    let removetextsasText = localStorage.getItem('removetexts');
-
-    if (titleAstext && textAstext && removetitlesAsText && removetextsasText) {
-        titles = JSON.parse(titleAstext);
-        texts = JSON.parse(textAstext);
-        removetitles = JSON.parse(removetitlesAsText);
-        removetexts = JSON.parse(removetextsasText);
-    }
-}
-
-function save() {
-    let titleAstext = JSON.stringify(titles);
-    let textAstext = JSON.stringify(texts);
-    let removetitlesAsText = JSON.stringify(removetitles);
-    let removetextsasText = JSON.stringify(removetexts);
-
-    localStorage.setItem('titles', titleAstext);
-    localStorage.setItem('texts', textAstext);
-    localStorage.setItem('removetitles', removetitlesAsText);
-    localStorage.setItem('removetexts', removetextsasText);
 }
 
 function removeNote(i) {
-    if (document.getElementById('headline').innerHTML === 'Today´s To Do´s') {
-        addTrashNote(i);
-        titles.splice(i, 1);
-        texts.splice(i, 1);
-    } else {
-        removetitles.splice(i, 1);
-        removetexts.splice(i, 1);
-    }
-    render();
-    save();
-}
-
-function addTrashNote(i) {
-    let deletedTitles = JSON.parse(localStorage.getItem('removetitles')) || [];
-    let deletedTexts = JSON.parse(localStorage.getItem('removetexts')) || [];
-    deletedTitles.push(titles[i]);
-    deletedTexts.push(texts[i]);
-    localStorage.setItem('removetitles', JSON.stringify(deletedTitles));
-    localStorage.setItem('removetexts', JSON.stringify(deletedTexts));
-    // Entferne die gelöschte Notiz aus dem Array
+    removetitles.push(titles[i]);
+    removetexts.push(texts[i]);
     titles.splice(i, 1);
     texts.splice(i, 1);
+
     render();
     save();
 }
 
+function deleteForever(i) {
+    removetitles.splice(i, 1);
+    removetexts.splice(i, 1);
+
+    save();
+    render();
+}
 
 function restore(i) {
     titles.push(removetitles[i]);
     texts.push(removetexts[i]);
-    removetitles.splice(i, 1);
-    removetexts.splice(i, 1);
-    render();
-    save();
+
+    deleteForever(i);
 }
 
-function editNote(i) {
+function editNote(i, isTrash) {
     let noteBox = document.getElementById('edit');
     showEdit();
 
-    let title = titles[i];
-    let text = texts[i];
+    let title = isTrash ? removetitles[i] : titles[i];
+    let text = isTrash ? removetexts[i] : texts[i];
 
     noteBox.innerHTML = `
     <textarea class="titleEdit" id="titleEdit" cols="40" rows="8">${title}</textarea>
     <textarea class="noteEdit" id="noteEdit" cols="40" rows="8">${text}</textarea>
-    <a href="#"><img src="./img/pin.png" alt="Sicherheitsnadel" onclick="editedNote(${i})"></a>
+    <a href="#"><img src="./img/pin.png" alt="Sicherheitsnadel" onclick="editedNote(${i}, ${isTrash})"></a>
     `;
 }
 
-function editedNote(i) {
+function editedNote(i, isTrash) {
     let title = document.getElementById('titleEdit').value;
     let note = document.getElementById('noteEdit').value;
-
-    titles[i] = title;
-    texts[i] = note;
-
+    if (isTrash) {
+        removetexts[i] = texts;
+        removetitles[i] = titles;
+    } else {
+        titles[i] = title;
+        texts[i] = note
+    }
     hideEdit();
     save();
     render();
@@ -155,18 +155,5 @@ function hideEdit() {
     document.getElementById('edit').classList.remove('editShow');
 }
 
-function loadDeletedNotes() {
-    let deletedTitles = JSON.parse(localStorage.getItem('removetitles')) || [];
-    let deletedTexts = JSON.parse(localStorage.getItem('removetexts')) || [];
-    
-    // Zeige die gelöschten Notizen an
-    for (let i = 0; i < deletedTitles.length; i++) {
-        const title = deletedTitles[i];
-        const text = deletedTexts[i];
-        // Hier kannst du den HTML-Code zum Anzeigen der gelöschten Notiz generieren und einfügen
-    }
-}
-
-// Rufe die Funktion zum Laden der gelöschten Notizen auf, wenn die Seite geladen wird
-window.onload = loadDeletedNotes;
-
+load();
+render();
